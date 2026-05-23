fb8697f4cb12eeaf6db9941dc73a77545d6555d5
Hamza <Hamza.Sinouh@outlook.de>
2026-05-23 21:39:35 +0200

    fixed reviews

--- src/lib/reviews.functions.ts
+++ src/lib/reviews.functions.ts
@@ -4,16 +4,28 @@ export const getReviews = async () => {
     total: 155,
     reviews: [
       {
-        author: "Katharina",
+        author: "katharina ries",
         rating: 5,
-        text: "Super leckeres Essen und tolle Atmosphäre.",
+        text: "Wir waren zum ersten Mal hier. Getränke kamen sehr schnell. Das Essen wurde frisch zubereitet und dauerte daher ein wenig. Es war super lecker. Top Preis Leistung. Restaurant und Toiletten sehr sauber. Wir kommen definitiv wieder.",
         relativeTime: "vor 7 Monaten",
       },
       {
-        author: "Edith",
+        author: "Edith Müller-Hofmann",
         rating: 5,
-        text: "Wir kommen immer wieder gerne.",
+        text: "Wir wohnen westlich von Köln und kommen immer gerne zu Bruno und seiner Frau. In der Kneipe gehört man auch bei den Stammgästen schnell dazu und wir werden immer herzlich begrüßt. Unser Lieblingsgericht ist das Bauernomlette mit Salat. Bruno weiß schon genau was wir mögen. Alles wird frisch gekocht und das schmeckt man einfach.",
         relativeTime: "vor 5 Monaten",
+      },
+      {
+        author: "daniel klemm",
+        rating: 5,
+        text: "Tolle Kneipe mit leckerem Essen, super Getränken und einem sehr herzlichen Kneiper. Preis Leistung 10/10. Sehr zu empfehlen.",
+        relativeTime: "vor 6 Monaten",
+      },
+      {
+        author: "mood4food by Christina",
+        rating: 5,
+        text: "We were in Cologne for three days and living next door from Bei Bruno. We were impressed by the simplicity, authenticity and quality of the food.",
+        relativeTime: "vor 2 Jahren",
       },
     ],
     error: null,
